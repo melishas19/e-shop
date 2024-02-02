94be371f3509e9e31a3dabb0fdb420d51db9871a
Melisha Shakya <melishas@buffalo.edu>
2024-02-01 20:13:57 -0800

    created home-product page with the given API (#1)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({product}) => {
+  console.log(product);
+  //destructure product
+  const { id, image, category, title, price } = product;
+
+  return (
+    <div>
+      <div className='each-product'>
+        <div className='within-product'>
+          {/*image*/}
+          <div className='img-product'>
+            <img className='img' src={image} alt='' />
+          </div>
+        </div>
+        {/* buttons */}
+        <div className='container-button'>
+          <button>
+            <div className="add-button"><b>+</b></div>
+          </button>
+          <hr className='text-white w-12'></hr>
+          <Link to={`/product/${id}`}>
+            <div><i className='fa fa-eye'></i></div>
+          </Link>
+        </div>
+      </div>
+      {/* category and title and price */}
+      <div>
+        <div className='category-text'>{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className='title-text'>{title}</h2>
+        </Link>
+        <div className='price-text'>{price}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
+//import product context
+import { ProductContext } from '../contexts/ProductContext';
+//import companents
+import Product from '../components/Product';
 
 const Home = () => {
-  return <div>Homepage</div>;
+  //get product form product context
+  const { products } = useContext(ProductContext);
+  console.log(products);
+  //get only men's and women's clothing category
+  const filteredProducts = products.filter(item => {return item.category === "men's clothing" || item.category === "women's clothing"});
+  console.log(filteredProducts);
+  return <div>
+    <section className='py-16'>
+      <div className="container-box">
+        <div className='container-product'>
+          {filteredProducts.map(product =>{
+            return <Product product={product} key={product.id} />
+          })}
+        </div>
+      </div>
+    </section>
+  </div>;
 };
 
 export default Home;
